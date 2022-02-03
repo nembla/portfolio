595ca2d4405f35cfc73f78ab9374bbd1a45f33f3
Blagojevic, Nemanja <nblagojevic3@gatech.edu>
2022-02-03 13:27:24 -0500

    cleanup + name

---
+++ src/components/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="name">Nemanja Blagojevic</div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
