7d35c3dc68ce332aecfb94dcd45f22c9bc81f517
Blagojevic, Nemanja <nblagojevic3@gatech.edu>
2022-02-04 14:21:24 -0500

    main page setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,150 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="navleft">
+      <router-link to="/">Nemanja Blagojevic</router-link>
+    </div>
+    <div class="navright">
+      <router-link to="/experience">Experience</router-link>
+      <router-link to="/projects">Projects</router-link>
+      <router-link to="/resume">Resume</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
   </div>
   <router-view />
 </template>
-
 <style>
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #f0ebd8;
+  background-color: #1d2d44;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
 }
 
 #nav {
-  padding: 30px;
+  padding: 20px;
+  display: flex;
+  padding-left: 4%;
+  background-color: white;
+  color: #0d1321;
+}
+
+.navleft {
+  min-width: 20%;
+  color: black;
+  font-size: 1.4em;
+  padding-left: 5%;
+}
+
+.navright {
+  margin-left: auto;
+  min-width: 35%;
+  padding-right: 5%;
+  margin-top: 0.25em;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #0d1321;
+  margin: 0% 3%;
+  text-decoration: none;
+  transition: 100ms;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.navright a.router-link-exact-active {
+  color: #748cab;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,151 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <div class="left">
+      <div class="profileimg">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
+          alt="placeholder profile picture"
+        />
+      </div>
+      <div class="name">
+        <p id="myName">Nemanja Blagojevic</p>
+        <p id="myTitle">Software Engineer</p>
+      </div>
+    </div>
+    <div class="right">
+      <div class="righttitle">Biography</div>
+      <div class="rightcontent">
+        <p>Hello my name is Nemanja, I am an aspiring software engineer.</p>
+        <p>
+          I am currently working at Rheem manufacturing as an Electronic
+          Controls engineer, as part of the systems engineering team.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          quisquam doloremque illum nulla sint quas et ut officiis vel cumque
+          enim ipsa, odio ipsam at quae ad hic aliquam molestiae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          quisquam doloremque illum nulla sint quas et ut officiis vel cumque
+          enim ipsa, odio ipsam at quae ad hic aliquam molestiae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          quisquam doloremque illum nulla sint quas et ut officiis vel cumque
+          enim ipsa, odio ipsam at quae ad hic aliquam molestiae.
+        </p>
+      </div>
+      <div class="rightinfo">
+        <div class="rightinfolist">
+          <div class="rightinfolisttittle">Interests</div>
+          <div class="interests">
+            <ul>
+              <li>Front End Web Development</li>
+              <li>Software Engineering</li>
+              <li>Machine Learning</li>
+              <li>Video Game Industry</li>
+            </ul>
+          </div>
+        </div>
+        <div class="rightinfolist">
+          <div class="rightinfolisttittle">Education</div>
+          <div class="education">
+            <ul>
+              <li>
+                <i class="fas fa-user-graduate">MS in Computer Science</i>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+
+<style>
+html {
+  margin: 0;
+  padding: 0;
+}
+
+.main {
+  min-height: 90vh;
+  margin: auto;
+  background-color: #1d2d44;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 0 2em;
+}
+
+#myName {
+  font-size: 2em;
+  margin-bottom: 10px;
+}
+
+#myTitle {
+  opacity: 50%;
+}
+
+.left {
+  margin-top: 5%;
+  flex: 2;
+}
+
+.right {
+  margin-top: 5%;
+  flex: 2;
+  margin-right: 10%;
+  text-align: left;
+}
+
+.righttitle {
+  font-size: 3em;
+  margin-bottom: 20px;
+}
+
+.profileimg img {
+  margin-bottom: 7.5%;
+  max-width: 25%;
+  border-radius: 50%;
+}
+
+.rightinfo {
+  margin: auto;
+  margin-top: 5%;
+  display: flex;
+  flex-direction: row;
+}
+
+.rightinfolist {
+  flex: 1;
+  text-align: center;
+}
+
+.rightinfolisttittle {
+  font-size: 1.3em;
+}
+
+.interests {
+  margin-left: 10%;
+  text-align: left;
+}
+
+.interests ul li {
+  margin-top: 5px;
+}
+
+.education {
+  margin-left: 10%;
+  text-align: left;
+}
+
+.education ul li {
+  margin-top: 5px;
+}
+</style>
